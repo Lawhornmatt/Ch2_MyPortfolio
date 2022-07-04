57ade4263cd74bb64ae3d691b92ce6336065e681
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-07-03 21:35:22 -0500

    Copious Changes. I should commit more often.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,10 @@
-//Put cool stuff here later
+//I want to make the page:
+//1) Scroll down to my pic, nav, and proj links upon load after a small delay
+//2) Scroll to the appropriate section when clicking the nav links instead of teleporting
+// $("article").find("a").click(function(e) {
+//     e.preventDefault();
+//     var section = $(this).attr("href");
+//     $("html, body").animate({
+//         scrollTop: $(section).offset().top
+//     });
+// });
